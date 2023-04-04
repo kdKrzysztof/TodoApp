4725d696b2ae756685a0c78ea628a56bf4e36c50
Krzysiek <supermax865@gmail.com>
2023-04-04 16:02:33 +0200

    refactor: renamed route from /users to /auth

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,12 @@
 import Express, { json } from 'express';
-import userController from './controllers/user.controller';
+import userController from './src/controllers/user.controller';
 
 const port = process.env.PORT || 5000;
 
 const app = Express();
 app.use(json());
 
-app.use('/users', userController);
+app.use('/auth', userController);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
