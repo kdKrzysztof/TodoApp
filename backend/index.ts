cd7af5d280ef909b31e7ac20bfd9598d1faf2dd4
Krzysiek <supermax865@gmail.com>
2023-04-05 18:48:49 +0200

    fix: added missing export

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,6 @@ import Express, { json } from 'express';
 import userController from './src/controllers/user.controller';
 
 const port = process.env.PORT || 5000;
-
 const app = Express();
 app.use(json());
 
@@ -11,3 +10,5 @@ app.use('/auth', userController);
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+export default app;
