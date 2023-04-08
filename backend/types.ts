25f48c70d0a641ec629b5f2fc7416eb4ce3d8eb4
Krzysiek <supermax865@gmail.com>
2023-04-08 12:18:58 +0200

    feat: added interface for login + register body

---
+++ backend/types.ts
@@ -0,0 +1,10 @@
+export interface register_userData {
+    username: string,
+    email: string,
+    password: string
+}
+
+export interface login_userData {
+    email: string,
+    password: string
+}
