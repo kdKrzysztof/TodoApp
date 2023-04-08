971963008d3d2e2232e898af626b10a86d6916cd
Krzysiek <supermax865@gmail.com>
2023-04-08 12:20:27 +0200

    refactor: removed catch from jwt script

--- backend/src/scripts/generateJWT.ts
+++ backend/src/scripts/generateJWT.ts
@@ -1,19 +1,16 @@
 import jwt from 'jsonwebtoken';
 import refreshTokenModel from '../sequelize/models/refreshToken.model';
 import cryptoRandomString from 'crypto-random-string';
-import { errorHandler } from '../middleware/errorHandler';
-import { Router } from 'express';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const router = Router();
-
-function addHours(date: Date, hours: number) {
+const addHours = (hours: number) => {
+    const date = new Date();
     date.setTime(date.getTime() + hours * 60 * 60 * 1000);
 
     return date;
-}
+};
 
 const generateJWT = async (userId: number, userEmail: string) => {
     const dataToSign = {
@@ -27,37 +24,21 @@ const generateJWT = async (userId: number, userEmail: string) => {
         expiresIn: process.env.TOKEN_LIFE,
     });
 
-    await refreshTokenModel
-        .destroy({
-            where: {
-                userId: userId,
-            },
-        })
-        .catch(() => {
-            throw new Error('Problem occured while deleting old refresh token');
-        });
-
-    const expiresAt = addHours(new Date(), 4);
-
-    await refreshTokenModel
-        .create({
+    await refreshTokenModel.destroy({
+        where: {
             userId: userId,
-            refreshToken: newRefreshToken,
-            expiration: expiresAt,
-        })
-        .catch(() => {
-            throw new Error('Problem occured while creating auth token');
-        });
+        },
+    });
 
-    const response = {
-        status: 'Logged in',
-        token,
-        newRefreshToken,
-    };
+    const expiresAt = addHours(4);
 
-    return response;
+    await refreshTokenModel.create({
+        userId: userId,
+        refreshToken: newRefreshToken,
+        expiration: expiresAt,
+    });
+
+    return {token, newRefreshToken}
 };
-
-router.use(errorHandler);
 
 export default generateJWT;
