e74ea304bfa99115a74846d2aeec9d358d58215f
Krzysiek <supermax865@gmail.com>
2023-04-17 17:37:44 +0200

    feat: improved routes

--- backend/src/controllers/todo/todo.controller.ts
+++ backend/src/controllers/todo/todo.controller.ts
@@ -15,16 +15,8 @@ const router = Router();
 
 router.use(authMiddleware);
 
-router.get('/GET', async (req: JwtPayload, res: Response) => {
+router.get('/', async (req: JwtPayload, res: Response) => {
     try {
-        const validationErrors =
-            validationResult(req).formatWith(errorFormatter);
-        if (!validationErrors.isEmpty()) {
-            return res.status(400).send({
-                errors: validationErrors.array(),
-            });
-        }
-
         const token = req.token;
 
         if (token.id === null || undefined) {
@@ -59,41 +51,68 @@ router.get('/GET', async (req: JwtPayload, res: Response) => {
     }
 });
 
-router.post(
-    '/ADD',
-    todoAddValidation,
-    async (req: JwtPayload, res: Response) => {
-        try {
-            const validationErrors =
-                validationResult(req).formatWith(errorFormatter);
-            if (!validationErrors.isEmpty()) {
-                return res.status(400).send({
-                    errors: validationErrors.array(),
-                });
-            }
-
-            const token = req.token;
-
-            if (token.id === null || undefined) {
-                throw new statusError('Invalid token parameters', 400);
-            }
-
-            const userData: addTodo = req.body;
-
-            await todoModel.create({
-                userId: token?.id,
-                title: userData.Title,
-                desc: userData.Desc,
-                expiresIn: userData.expiresIn,
+router.post('/', todoAddValidation, async (req: JwtPayload, res: Response) => {
+    try {
+        const validationErrors =
+            validationResult(req).formatWith(errorFormatter);
+        if (!validationErrors.isEmpty()) {
+            return res.status(400).send({
+                errors: validationErrors.array(),
             });
-
-            res.sendStatus(200);
-        } catch (err) {
-            console.error((err as Error).stack);
-            throw err;
         }
+
+        const token = req.token;
+
+        if (token.id === null || undefined) {
+            throw new statusError('Invalid token parameters', 400);
+        }
+
+        const userData: addTodo = req.body;
+
+        await todoModel.create({
+            userId: token?.id,
+            title: userData.Title,
+            desc: userData.Desc,
+            expiresIn: userData.expiresIn,
+        });
+
+        res.sendStatus(204);
+    } catch (err) {
+        console.error((err as Error).stack);
+        throw err;
     }
-);
+});
+
+router.delete('/:id', async (req: JwtPayload, res: Response) => {
+    try {
+        const token = req.token;
+        if (!token.id) {
+            throw new statusError('Invalid token parameters', 400);
+        }
+
+        const todoId = req.params.id;
+        if (!todoId) {
+            throw new statusError(`TodoId can't be null or undefined`, 400);
+        }
+
+        const foundTodoItem = await todoModel.findOne({
+            where: {
+                userId: token.id,
+                todoId: todoId,
+            },
+        });
+
+        if (foundTodoItem) {
+            foundTodoItem.destroy();
+            res.sendStatus(204);
+        } else {
+            throw new statusError('Todo item not found', 404);
+        }
+    } catch (err) {
+        console.log((err as Error).stack);
+        throw err;
+    }
+});
 
 router.use(errorHandler);
 
