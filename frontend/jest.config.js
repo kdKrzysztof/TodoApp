1c07fb137173cda0dbb9e72273a90c4217cd85a1
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-05-02 12:46:19 +0200

    test: setup Jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,7 @@
+export default {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
