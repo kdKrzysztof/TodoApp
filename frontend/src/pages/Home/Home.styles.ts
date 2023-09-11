98b9066082f77670c9ca624964660da065352b33
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-11 11:46:52 +0200

    feat: separate file for Home utils and styles

---
+++ frontend/src/pages/Home/Home.styles.ts
@@ -0,0 +1,32 @@
+import { Container, ListItem, Fab, ListItemText } from '@mui/material';
+import { styled } from '@mui/system';
+
+export const ListMainBody = styled(Container)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100vw',
+  marginTop: '2.5rem'
+});
+
+export const CustomListItem = styled(ListItem)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  columnGap: '1rem'
+});
+
+export const CustomFab = styled(Fab)({
+  margin: 0,
+  top: 'auto',
+  right: 30,
+  bottom: 30,
+  left: 'auto',
+  position: 'fixed'
+});
+
+export const CustomListItemButton = styled(ListItemText)({
+  ['.MuiListItemText-primary']: {
+    fontSize: '1.1rem'
+  }
+});
