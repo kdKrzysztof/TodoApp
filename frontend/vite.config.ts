dbe14b9b15429509603a41ee0165b3546a3e0384
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-11 13:13:23 +0200

    feat: define absolute paths in project config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+      components: path.resolve(__dirname, './src/components'),
+      utils: path.resolve(__dirname, './src/utils'),
+      pages: path.resolve(__dirname, './src/pages')
+    }
+  },
   server: {
     proxy: {
       '/api': {
