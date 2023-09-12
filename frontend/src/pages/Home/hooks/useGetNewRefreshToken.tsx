12b3ac82e8d42bc830943de72c2069aba60ef712
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-12 12:56:05 +0200

    refactor: rename GetNewRefershTokenParams

--- frontend/src/pages/Home/hooks/useGetNewRefreshToken.tsx
+++ frontend/src/pages/Home/hooks/useGetNewRefreshToken.tsx
@@ -5,14 +5,14 @@ import { useNavigate } from 'react-router-dom';
 
 import { getNewRefreshToken } from 'src/utils/getNewRefreshToken';
 
-interface GetNewRefershTokenParams {
+interface GetNewRefershTokenProps {
   isError: boolean;
   refetch: <TPageData>(
     options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined
   ) => Promise<QueryObserverResult<AxiosResponse<any, any>, unknown>>;
 }
 
-const useGetNewRefreshToken = ({ isError, refetch }: GetNewRefershTokenParams) => {
+const useGetNewRefreshToken = ({ isError, refetch }: GetNewRefershTokenProps) => {
   const navigate = useNavigate();
   useEffect(() => {
     if (isError) {
