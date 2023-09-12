0981665d68219791331b7deaa80d4effda8f9f75
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-12 13:00:06 +0200

    refactor: move custom component to .styles

---
+++ frontend/src/components/AddTodoForm/AddTodoForm.styles.ts
@@ -0,0 +1,7 @@
+import { DialogContent, styled } from '@mui/material';
+
+export const CustomDialogContent = styled(DialogContent)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+});
